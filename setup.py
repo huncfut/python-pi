3e4b9e0a6adbcfe29cbc20f3c4f1533643e9506d
Kuba Zeligowski <kzeligowski@MacBook-Pro-Kuba.local>
2018-01-26 09:40:25 +0100

    Fixed minor issues (import)

--- setup.py
+++ setup.py
@@ -1,46 +1,42 @@
-# import RPi.GPIO as GPIO
-# import time
+import RPi.GPIO as GPIO
+import time
 import json
+import threading
 from pprint import pprint
+
 import threading
 
-# GPIO.setmode(GPIO.BCM)
-def turnOn(compNum):
-    print(compNum)
-    return 0
-
-def runTurnOn(turnOnList):
-    for element in turnOnList:
-        turnOn(comPins[element-1])
-
 comPins = [17, 18, 19, 20, 21, 22, 23, 24]
+GPIO.setmode(GPIO.BCM)
+
+
+def turnOn(compNum):
+    GPIO.output(compNum, 1)
+    time.sleep(0.5)
+    GPIO.output(compNum, 0)
+
+def turnOff(numOfComp):
+    GPIO.output(numOfComp, 1)
+    time.sleep(0.5)
+    GPIO.output(numOfComp, 0)
+
+def turnHardOff(numOfComp):
+    GPIO.output(numOfComp, 1)
+    time.sleep(5)
+    GPIO.output(numOfComp, 0)
 
 
 def setup():
     threading.Timer(10.0, setup).start()
+
     instructionJson = json.load(open('kupa.json'))
-    runTurnOn(instructionJson["turnOn"])
+
+    for compNum in instructionJson["turnOn"]:
+        turnOn(comPins[compNum-1])
+    for compNum in instructionJson["turnOff"]:
+        turnOff(comPins[compNum-1])
+    for compNum in instructionJson["turnHardOff"]:
+        turnHardOff(comPins[compNum-1])
+
 
 setup()
-
-
-
-
-# def turnOn(numOfComp):
-#     GPIO.output(numOfComp, GPIO.HIGH)
-#     time.sleep(0.5)
-#     GPIO.output(numOfComp, GPIO.LOW)
-#
-# def turnOff(numOfComp):
-#     GPIO.output(numOfComp, GPIO.HIGH)
-#     time.sleep(0.5)
-#     GPIO.output(numOfComp, GPIO.LOW)
-#
-# def turnHardOff(arg):
-#     GPIO.output(numOfComp, GPIO.HIGH)
-#     time.sleep(5)
-#     GPIO.output(numOfComp, GPIO.LOW)
-#
-# while True:
-#     compNum = int(input("Computer #"))
-#     funcNum = int(input("Function:\n1 => turn on\n2 => turn off\n3 => hard reset\n"))
